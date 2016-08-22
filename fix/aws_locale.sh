6d8de5ca008db016e6af79d4cc1376bb3249fbe5
Dexter <c3zar@protonmail.ch>
2016-08-22 20:35:19 +0200

    locale fix

--- fix/aws_locale.sh
+++ fix/aws_locale.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# run as root only
+if [[ $EUID -ne 0 ]] ; then
+    echo "This script must be run with root access\e[49m"
+    exit 1
+fi
 
-export LC_ALL="en_US.UTF-8"
+echo LC_ALL="en_US.UTF-8" >> /etc/environment
 locale-gen "en_US.UTF-8"
 dpkg-reconfigure locales
+# Reboot
