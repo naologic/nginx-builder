bd65cbe24485fd3816a8cc520d2d3c05fc4f4399
Gabriel <gabi.paunescu@gmail.com>
2017-01-26 20:30:33 +0200

    ensure the pid is there

--- app/libs.sh
+++ app/libs.sh
@@ -199,7 +199,10 @@ function post_install_nginx() {
       # Paths 
         sed -i -e "s|\$REPLACE_NGINX_PATH|${NGINX_PATH}|g" /etc/init.d/nginx
         sed -i -e "s|\$REPLACE_NGINX_USE_PATH|${NGINX_USE_PATH}|g" /etc/init.d/nginx
-
+    # Touch: pid
+    touch /run/nginx.pid
+    chmod 0770 /run/nginx.pid
+    
     if [ $DISTRO_VERSION = "16.04" ] || [ $DISTRO_VERSION = "16.10" ] ; then 
         mkdir -p /usr/lib/systemd/system/
         # Service file for nginx
